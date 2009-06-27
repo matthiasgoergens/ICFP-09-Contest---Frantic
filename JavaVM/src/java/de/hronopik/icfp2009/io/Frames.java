fb539bf49be00b4ae0f32c5888f4640c333c1c70
akiel <akiel@7a3dea4a-0a21-4dba-9d8b-748cb24728c7>
2009-06-27 16:09:45 +0000

    new package structure
    git-svn-id: https://svn.hronopik.de/icfp2009/trunk@86 7a3dea4a-0a21-4dba-9d8b-748cb24728c7

---
+++ JavaVM/src/java/de/hronopik/icfp2009/io/Frames.java
@@ -0,0 +1,31 @@
+package de.hronopik.icfp2009.io;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alexander Kiel
+ * @version $Id$
+ */
+public class Frames {
+
+    @NotNull
+    public static List<Frame> readFromFile(@NotNull File file) throws IOException {
+        OrbitBinaryInputStream in = new OrbitBinaryInputStream(new FileInputStream(file));
+        List<Frame> frames = new ArrayList<Frame>();
+        try {
+            while (true) {
+                frames.add(in.readFrame());
+            }
+        } catch (EOFException e) {
+            in.close();
+            return frames;
+        }
+    }
+}
