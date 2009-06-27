cf7a179c77beb5309bce4af6069b0d023e4cd76a
dex <dex@7a3dea4a-0a21-4dba-9d8b-748cb24728c7>
2009-06-27 18:29:44 +0000

    single is able to change the velocity now and makes the vm to behave badly
    git-svn-id: https://svn.hronopik.de/icfp2009/trunk@96 7a3dea4a-0a21-4dba-9d8b-748cb24728c7

--- controller/single.pl
+++ controller/single.pl
@@ -16,10 +16,17 @@ WRITE->autoflush(1);
 
 print WRITE "16000 $CFG\n";
 
+$std_command = sub{
+  print WRITE ".\n";
+};
+
+$command = $std_command;
+
 for( my $i=0; $i < $maxstep ; $i++){
 # print "$i\n";
-    print WRITE ".\n";
-        
+    
+    $command->();
+            
     while( ($line = <READ>) ne ".\n" ){
       chomp $line;
 #      print "<<<$line>>>\n";
@@ -40,4 +47,42 @@ for( my $i=0; $i < $maxstep ; $i++){
     print STDERR "reg1: $register{1}\n";
     print STDERR "reg2: $register{2}\n";
     print STDERR "reg3: $register{3}\n";
+    
+    $fuel = $register{1};
+    $sx = $register{2};
+    $sy = $register{3};
+    $target_orbit_radius = $register{4};
+    
+    if(defined $last_sx && defined $last_sy){
+      $vx = $last_sx - $sx;
+      $vy = $last_sy - $sy;
+      $v = sqrt( ($sx - $last_sx)**2 + ($sy - $last_sy)**2 );
+      
+      $m_e = 6E24;
+      $r_e = 6.357E6;
+      $G = 6.67428E-11;
+      
+      $GM = 398600.4418;
+      
+      $r = sqrt( $sx**2 + $sy**2 );
+      
+      print STDERR "speed: $v, radius: $r";
+    }
+    
+    if( $time == 2 ){
+      my $nvx = -$sy * 1/$r * 2466.49;
+      my $nvy = -$sx * 1/$r * 2466.49;      
+      
+      $command = sub{
+        print WRITE "2 $nvx\n";
+        print WRITE "3 $nvy\n";
+        print WRITE "16000 $CFG\n";
+        print WRITE ".\n";
+        
+        $command = $std_command;
+      };
+    }
+    
+    $last_sx = $sx;
+    $last_sy = $sy;
 } 
