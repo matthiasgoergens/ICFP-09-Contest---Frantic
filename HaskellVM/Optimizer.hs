df735e2f59a98efb997bf833a255192a2dcd0648
georg <georg@7a3dea4a-0a21-4dba-9d8b-748cb24728c7>
2009-06-28 10:23:17 +0000

    optimizer 1D works
    git-svn-id: https://svn.hronopik.de/icfp2009/trunk@114 7a3dea4a-0a21-4dba-9d8b-748cb24728c7

--- HaskellVM/Optimizer.hs
+++ HaskellVM/Optimizer.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FlexibleInstances #-}
 
 module Optimizer where
 
@@ -16,24 +15,11 @@ type Time = Int
 type Opt = Double
 type TOpt = (Time,Double)
 
-{-
-
-vm <- loadVMFromFile "../task/bin1.obf"    
-let (vm',r) = testN vm init1_1 opt1_1 10    
-let (vm',r) = test vm init1_1 opt1_1 crit1_1
-getOptTimeMin r
-optimizer2 vm init1_1 1 0.001 opt1_1 crit1_1 getOptTimeMin
-
-vm <- loadVMFromFile "../task/bin1.obf"    
-optimizer1 defParams vm init1_1 3 opt1_1a crit1_1 getOptTimeMin
-
-
--}
-
 type CritFun = ((Outp,Outp) -> Time -> Bool)
 type OptFun = ((Outp,Outp) -> Time -> Opt)
 type FitFun = [TOpt] -> (Time, Opt)
     
+---- Functions to test criterion and OptFun
 
 test :: VM -> [(Addr,Dat)] -> OptFun -> CritFun -> (VM, [TOpt])
 test vm cmds optfun abortcrit =
@@ -47,6 +33,8 @@ test vm cmds optfun abortcrit =
                   in case abb of 
                        True  -> (vm', opts)
                        False -> helper rest ((time,opt):opts) out vm'                     
+
+---- Functions to test OptFun for N steps
 
 -- fixed number of steps
 testN :: VM -> [(Addr,Dat)] -> OptFun -> Time -> (VM, [TOpt])
@@ -62,6 +50,51 @@ testN vm cmds optfun maxtime =
                        True  -> (vm', opts)
                        False -> helper rest ((time,opt):opts) out vm'
 
+--- Function to interate N steps
+
+getN :: VM -> [(Addr,Dat)] -> Time -> VM
+getN vm cmds  maxtime =
+    let inps = take maxtime $ (Inp $ I.fromList cmds) : (repeat $ Inp I.empty) 
+    in foldl (\v i -> fst $ oneRun i v  ) vm inps
+
+
+-------  Gradient Descent Algos 
+--- Params
+data OptParams =  OptParams { eps :: Dat
+                            , delta :: Dat
+                            , thresh :: Opt 
+                            , maxiter :: Int
+                            } deriving (Show)
+
+defParams =  OptParams { eps =0.1
+                       , delta = 0.0001
+                       , thresh = 10 
+                       , maxiter = 10
+                       } 
+
+---  Gradient Descent for 1 Param
+
+-- optimizes parameter at Addr to minimize FitFun
+optimizer1 :: OptParams -> VM -> [(Addr,Dat)] -> Addr -> OptFun -> CritFun -> FitFun -> ([(Addr,Dat)],Dat,Opt,Time)
+optimizer1 params vm cmds port optfun abortcrit fitfun =
+    let (_,r) = test vm cmds optfun abortcrit
+        (t,opt) = fitfun r
+        steps   = iterate loop (cmds, delta params, opt, t)
+        optsteps = takeWhile ( (> (thresh params)) . trd4 ) $ take (maxiter params) $ steps        
+    in minimumBy (compare `on` trd4) $ trace (unlines $ map (show . trd4) optsteps) $ optsteps
+ where loop :: ([(Addr,Dat)], Dat, Opt, Time) -> ([(Addr,Dat)], Dat, Opt, Time)
+       loop (cmds,h,opt, t) = 
+           let cmdchange = mapsnd (\x -> h) $ head $ filter ( (== port) . fst ) cmds
+               cmds'     = I.toList $ I.fromListWith (+) (cmdchange : cmds )        
+               (delta',opt',t') = helper cmds' h opt
+               in (cmds',delta',opt',t')
+       helper cmds h oldopt = 
+           let (_,r') = test vm cmds optfun abortcrit
+               (t',opt') = fitfun r'
+               grad     = (opt'-oldopt) / h
+           in  (-(eps params)* oldopt/grad, opt',t')
+          
+
 optimizer :: VM -> [(Addr,Dat)] -> Int -> OptFun -> CritFun -> FitFun -> (Opt,[(Addr,Dat)])
 optimizer vm cmds numparams optfun abortcrit fitfun=
     let (_,r) = test vm cmds optfun abortcrit
@@ -74,43 +107,7 @@ optimizer vm cmds numparams optfun abortcrit fitfun=
     in (grad,[])
 
 
-data OptParams =  OptParams { eps :: Dat
-                            , delta :: Dat
-                            , thresh :: Opt 
-                            , maxiter :: Int
-                            } deriving (Show)
 
-defParams =  OptParams { eps =0.3
-                       , delta = 0.0001
-                       , thresh = 10 
-                       , maxiter = 10
-                       } 
-
-optimizer1 :: OptParams -> VM -> [(Addr,Dat)] -> Addr -> OptFun -> CritFun -> FitFun -> ([(Addr,Dat)],Dat,Opt)
-optimizer1 params vm cmds port optfun abortcrit fitfun =
-    let (_,r) = test vm cmds optfun abortcrit
-        (t,opt) = fitfun r
-        steps   = iterate loop (cmds, delta params, opt)
-        optsteps = takeWhile ( (> (thresh params)) . trd ) $ take (maxiter params) $ trace (show $ take 10 steps) steps        
-    in minimumBy (compare `on` trd) optsteps
- where loop :: ([(Addr,Dat)], Dat, Opt) -> ([(Addr,Dat)], Dat, Opt)
-       loop (cmds,h,opt) = 
-           let cmdchange = mapsnd (\x -> h) $ head $ filter ( (== port) . fst ) cmds
-               cmds'     = I.toList $ I.fromListWith (+) (cmdchange : cmds )        
-               (delta',opt') = helper cmds' h opt
-               in (cmds',delta',opt')
-       helper cmds h oldopt = 
-           let (_,r') = test vm cmds optfun abortcrit
-               (_,opt') = fitfun r'
-               grad     = (opt'-oldopt) / h
-           in  (-(eps params)* oldopt/grad, opt')
-          
-
-
-getN :: VM -> [(Addr,Dat)] -> Time -> VM
-getN vm cmds  maxtime =
-    let inps = take maxtime $ (Inp $ I.fromList cmds) : (repeat $ Inp I.empty) 
-    in foldl (\v i -> fst $ oneRun i v  ) vm inps
     
 getRunTime :: [TOpt] -> Time
 getRunTime opts = fst $ head opts
@@ -123,19 +120,6 @@ getOptTimeMax :: [TOpt] -> (Time, Opt)
 getOptTimeMax opts =  maximumBy (\ (_,o1) (_,o2) ->  compare o1 o2 ) $ opts
 
 ---
-
-instance Num a => Num (a,a) where
-    (+) (a,c) (b,d) = (a+b,c+d)
-    (-) (a,c) (b,d) = (a-b,c-d)
-    (*) (a,c) (b,d) = (a*b,c*d)
-    negate (a,b)    = (-a,-b)
-    abs (a,b)       = (abs(a),abs(b))
-    signum (a,b)    = (signum a, signum b) 
-    fromInteger i   = (fromInteger i, fromInteger i) 
-                      
-
-trd :: (a,b,c) -> c
-trd (_,_,c) = c
    
 get :: Int -> Outp -> Dat
 get k (Outp outp) = I.findWithDefault 0 k outp
@@ -145,13 +129,6 @@ getVel k (o1, o2) = get k o2 - get k o1
 
 getRad :: Outp -> Dat
 getRad o = sqrt $ (sqr $ get 2 o)  + (sqr $ get 3 o)
-
-init1_1 :: [(Addr,Dat)]
--- init1_1 = [(16000,1001),(3, -2466.4860122222)]
-init1_1 = [(16000,1001),(3, -2466.4860122222 - 1.3176340142438757e-5)]
-
-init1_1b :: [(Addr,Dat)]
-init1_1b = [(3, 1482.9355671460403)]
 
 
 isEmpty :: Outp -> Bool
@@ -174,14 +151,49 @@ hohmannSpeed2 :: Dat -> Dat -> Dat
 hohmannSpeed2 r1 r2 = 
     sqrt(mu / r2) * (1 - sqrt(2 * r1 / (r1 + r2)))
 
+printStat :: ([(Addr,Dat)],Dat,Opt,Time) -> IO()
+printStat (newcmds, _ , opt, time) = 
+    putStrLn $ "Opts and Time: " ++ (show time) 
+                 ++ "\nNewCommands: " ++ (show newcmds) 
+                 ++ "\nOpt: " ++ (show opt)
+
 
 ---------
+-- PROBLEM 1
 
-opt1_1 :: (Outp,Outp) -> Time -> Opt
-opt1_1 p@(old,o) _ = 
-    let rad  = getRad o
-        soll = get 4 o
-        in abs (rad - soll)
+{-
+
+
+vm <- loadVMFromFile "../task/bin1.obf"    
+let params = defParams { thresh = 150 }
+let stat@(newcmds, _ , opt, time) = optimizer1 defParams vm init1_1 3 opt1_1a crit1_1 getOptTimeMin
+printStat stat
+
+let vm2 = getN vm newcmds time
+let stat2 = optimizer1 defParams vm2 init1_1b 3 opt1_1b crit1_1b getOptTimeMax
+printStat stat2
+
+
+
+
+
+
+vm <- loadVMFromFile "../task/bin1.obf"    
+let (vm',r) = testN vm init1_1 opt1_1 10    
+let (vm',r) = test vm init1_1 opt1_1 crit1_1
+getOptTimeMin r
+optimizer2 vm init1_1 1 0.001 opt1_1 crit1_1 getOptTimeMin
+
+
+-}
+
+init1_1 :: [(Addr,Dat)]
+-- init1_1 = [(16000,1001),(3, -2466.4860122222)]
+init1_1 = [(16000,1001),(3, -2466.4860122222 - 1.3176340142438757e-5)]
+
+init1_1b :: [(Addr,Dat)]
+init1_1b = [(3, 1482.9355671460403)]
+
 
 -- optimal point
 opt1_1a :: (Outp,Outp) -> Time -> Opt
@@ -190,6 +202,14 @@ opt1_1a p@(old,o) _ =
         x = get 2 o
         y = get 3 o
         in vecLen ((x,y) - (sollr,0))
+
+-- optimal orbit
+opt1_1b :: (Outp,Outp) -> Time -> Opt
+opt1_1b p@(old,o) _ = 
+    let rad  = getRad o
+        soll = get 4 o
+        in abs (rad - soll)
+
 
 
 crit1_1 :: (Outp,Outp) -> Time -> Bool
