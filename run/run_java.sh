ebada1ef71fd5944d8e5632f597aa14c48a2132b
akiel <akiel@7a3dea4a-0a21-4dba-9d8b-748cb24728c7>
2009-06-28 12:19:55 +0000

    removed -ea
    git-svn-id: https://svn.hronopik.de/icfp2009/trunk@129 7a3dea4a-0a21-4dba-9d8b-748cb24728c7

--- run/run_java.sh
+++ run/run_java.sh
@@ -3,4 +3,4 @@
 VERSION=0.1
 LOGGING_CONFIG=logging.properties
 
-java -ea -Djava.util.logging.config.file=$LOGGING_CONFIG -jar ../JavaVM/dist/java_vm-$VERSION.jar $1
+java -Djava.util.logging.config.file=$LOGGING_CONFIG -jar ../JavaVM/dist/java_vm-$VERSION.jar $1
